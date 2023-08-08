05d16f40645ab4ef479723150e952a124d0fd70f
nedmoyle <140259915+nedmoyle@users.noreply.github.com>
2023-08-08 22:11:01 +1000

    Added functionality to the game

--- rps.js
+++ rps.js
@@ -1,13 +1,36 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 30) + 1;
-    console.log(randomNumber);
+    // console.log(randomNumber);
     if (randomNumber <= 10) {
-        return "Rock";
+        return "rock";
     } else if (randomNumber >20) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionLowercase = playerSelection.toLowerCase();
+    if (playerSelectionLowercase == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelectionLowercase == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelectionLowercase == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelectionLowercase == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelectionLowercase == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beat paper";
+    } else if (playerSelectionLowercase == computerSelection) {
+        return "You Draw";
+    } else return "Something went baddd"
+}
+
+const playerSelection = "rOck";
+console.log(playerSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 

7ed2356834700ae69653330faa75313d0fbee7fd
nedmoyle <140259915+nedmoyle@users.noreply.github.com>
2023-08-08 23:08:47 +1000

    complete first stage of rock paper scissors

--- rps.js
+++ rps.js
@@ -14,23 +14,56 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let playerSelectionLowercase = playerSelection.toLowerCase();
     if (playerSelectionLowercase == "rock" && computerSelection == "paper") {
-        return "You Lose! Paper beats Rock";
+        return -1;
     } else if (playerSelectionLowercase == "paper" && computerSelection == "rock") {
-        return "You Win! Paper beats Rock";
+        return 1;
     } else if (playerSelectionLowercase == "scissors" && computerSelection == "rock") {
-        return "You Lose! Rock beats Scissors";
+        return -1;
     } else if (playerSelectionLowercase == "rock" && computerSelection == "scissors") {
-        return "You Win! Rock beats Scissors";
+        return 1;
     } else if (playerSelectionLowercase == "paper" && computerSelection == "scissors") {
-        return "You Lose! Scissors beat paper";
+        return -1;
+    } else if (playerSelectionLowercase == "scissors" && computerSelection == "paper") {
+        return 1;
     } else if (playerSelectionLowercase == computerSelection) {
-        return "You Draw";
-    } else return "Something went baddd"
+        return 0;
+    }
 }
 
-const playerSelection = "rOck";
-console.log(playerSelection);
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+const playerSelection = prompt("What is your choice?");
+console.log(playerSelection.toLowerCase());
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let totalPoints = 0;
+    for (let i=1; i<6; i++){
+        console.log("Game: " + i + ":");
+        let computerSelection = getComputerChoice();
+        console.log("Computer chooses: " + computerSelection);
+        let thisRound = playRound(playerSelection, computerSelection);
+        totalPoints = totalPoints + thisRound;
+
+        if (thisRound == 1) {
+            console.log(playerSelection + " beats " + computerSelection);
+        } else if (thisRound == -1) {
+            console.log(computerSelection + " beats " + playerSelection);
+        } else if (thisRound ==0) {
+            console.log("This round was a draw");
+        }
+
+        console.log(totalPoints);
+
+    }
+    if (totalPoints > 0) {
+        return "You Win!";
+    } else if (totalPoints < 0 ) {
+        return "Computer Wins!";
+    } else if (totalPoints == 0) {
+        return "The game was a draw";
+    }
+}
+
+console.log(game());
 
